4ab57d50dfc339826596f050e0db699dc4eb33ba
julianalverio <julian.a.alverio@gmail.com>
2018-11-24 18:26:09 -0500

    iterating

--- fetch/gripper_enabled/prepare_to_grasp/fetch.py
+++ fetch/gripper_enabled/prepare_to_grasp/fetch.py
@@ -235,10 +235,10 @@ class Trainer(object):
         new_right_finger = copy.deepcopy(self.env.sim.data.get_joint_qpos('robot0:r_gripper_finger_joint'))
         new_left_finger = copy.deepcopy(self.env.sim.data.get_joint_qpos('robot0:l_gripper_finger_joint'))
         new_finger_distance = new_right_finger + new_left_finger
-        if finger_distance != new_finger_distance:
+        if abs(finger_distance - new_finger_distance) > 1e5:
             print(new_finger_distance - finger_distance)
-            # print(self.convertAction(action))
-            # import time; time.sleep(1)
+            print(self.convertAction(action))
+            import time; time.sleep(1)
 
         if done:
             self.memory.push(self.state, action, torch.tensor([reward], device=self.device), None)
