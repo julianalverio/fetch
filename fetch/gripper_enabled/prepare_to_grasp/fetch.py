4f4a7cdcbc71c15085e77dfb6fa84e8719c0cc33
julianalverio <julian.a.alverio@gmail.com>
2018-11-24 17:32:33 -0500

    iterating

--- fetch/gripper_enabled/prepare_to_grasp/fetch.py
+++ fetch/gripper_enabled/prepare_to_grasp/fetch.py
@@ -176,7 +176,7 @@ class Trainer(object):
             'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
         }
         env = fetch_env.FetchEnv('fetch/push.xml', has_object=True, block_gripper=False, n_substeps=20,
-            gripper_extra_height=0.0, target_in_the_air=False, target_offset=0.0,
+            gripper_extra_height=0.2, target_in_the_air=False, target_offset=0.0,
             obj_range=0.15, target_range=0.15, distance_threshold=0.05,
             initial_qpos=initial_qpos, reward_type='sparse')
         return TimeLimit(env)
