72975bc1491c8148e9c49c276d74c9e624ce3a48
julianalverio <julian.a.alverio@gmail.com>
2018-11-24 18:45:01 -0500

    iterating

--- fetch/gripper_enabled/prepare_to_grasp/fetch.py
+++ fetch/gripper_enabled/prepare_to_grasp/fetch.py
@@ -320,7 +320,7 @@ class Trainer(object):
     def train(self):
         import pdb; pdb.set_trace()
         for _ in range(100):
-            self.env.step([0,0,0,1])
+            self.env.step([0,0,0,-1])
         for _ in range(20):
             self.env.render()
         import pdb; pdb.set_trace()
