4879277d745ff30e2f551e078343a5da45ddf667
julianalverio <julian.a.alverio@gmail.com>
2018-11-24 18:49:38 -0500

    iterating

--- fetch/bad_cropping/gym/envs/robotics/utils.py
+++ fetch/bad_cropping/gym/envs/robotics/utils.py
@@ -26,13 +26,13 @@ def ctrl_set_action(sim, action):
     """
     if sim.model.nmocap > 0:
         _, action = np.split(action, (sim.model.nmocap * 7, ))
-    # if sim.data.ctrl is not None:
-    for i in range(action.shape[0]):
-        if sim.model.actuator_biastype[i] == 0:
-            sim.data.ctrl[i] = action[i]
-        else:
-            idx = sim.model.jnt_qposadr[sim.model.actuator_trnid[i, 0]]
-            sim.data.ctrl[i] = sim.data.qpos[idx] + action[i]
+    if sim.data.ctrl is not None:
+        for i in range(action.shape[0]):
+            if sim.model.actuator_biastype[i] == 0:
+                sim.data.ctrl[i] = action[i]
+            else:
+                idx = sim.model.jnt_qposadr[sim.model.actuator_trnid[i, 0]]
+                sim.data.ctrl[i] = sim.data.qpos[idx] + action[i]
 
 
 def mocap_set_action(sim, action):
