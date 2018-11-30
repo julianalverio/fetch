2fed2ca816a1b068935b25212cd35bab7eb6069c
julianalverio <julian.a.alverio@gmail.com>
2018-11-30 01:48:10 -0500

    iterating

--- fetch/annealing_and_camera_distance/rendering_bug/fetch.py
+++ fetch/annealing_and_camera_distance/rendering_bug/fetch.py
@@ -20,7 +20,7 @@ import os
 import sys
 # sys.path.pop(0)
 # import gym
-sys.path.insert(0, '/storage/jalverio/venv/fetch/fetch')
+sys.path.insert(0, '/storage/jalverio/venv/fetch/fetch/annealing_and_camera_distance/rendering_bug')
 from gym.envs.robotics import fetch_env
 from gym import utils
 from gym.wrappers.time_limit import TimeLimit
