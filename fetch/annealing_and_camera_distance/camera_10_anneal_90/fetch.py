2193f2d1bf0d2784aa3bdc49ede076f03f9b5774
julianalverio <julian.a.alverio@gmail.com>
2018-11-30 12:55:19 -0500

    iterating

--- fetch/annealing_and_camera_distance/camera_10_anneal_90/fetch.py
+++ fetch/annealing_and_camera_distance/camera_10_anneal_90/fetch.py
@@ -211,7 +211,7 @@ class Trainer(object):
         for y in range(height):
             for x in range(width):
                 r, g, b = data[x, y]
-                if r < 50 and g < 50 and b < 50:
+                if 100 < r < 50 and 100 < g < 50 and 100 < b < 50:
                     data[x, y] = (255, 0, 0)
         import pdb; pdb.set_trace()
         image.show()
