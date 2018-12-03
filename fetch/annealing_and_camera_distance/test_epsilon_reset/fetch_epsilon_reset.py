2a745f3bddfe8608337db3e36371732366b4210a
julianalverio <julian.a.alverio@gmail.com>
2018-12-03 14:56:08 -0500

    iterating

--- fetch/annealing_and_camera_distance/test_epsilon_reset/fetch_epsilon_reset.py
+++ fetch/annealing_and_camera_distance/test_epsilon_reset/fetch_epsilon_reset.py
@@ -196,7 +196,7 @@ class Trainer(object):
             'robot0:slide2': 0.0,
             'object0:joint': [1.25, 0.53, 0.4, 1., 0., 0., 0.],
         }
-        env = fetch_env.FetchEnv('fetch/push_slightly_cluttered.xml', has_object=True, block_gripper=True, n_substeps=20,
+        env = fetch_env.FetchEnv('fetch/push.xml', has_object=True, block_gripper=True, n_substeps=20,
             gripper_extra_height=0.2, target_in_the_air=False, target_offset=0.0,
             obj_range=0.15, target_range=0.15, distance_threshold=0.05,
             initial_qpos=initial_qpos, reward_type='sparse')
