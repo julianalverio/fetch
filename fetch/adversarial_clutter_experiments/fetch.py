86db0236cb5521ecbe19e0787a77ecf8f553b0bb
julianalverio <julian.a.alverio@gmail.com>
2018-12-03 22:30:08 -0500

    iterating

--- fetch/adversarial_clutter_experiments/fetch.py
+++ fetch/adversarial_clutter_experiments/fetch.py
@@ -194,7 +194,7 @@ class Trainer(object):
             'object2:joint': [1.30, 0.65, 0.4, 1., 0., 0., 0.],
             'object3:joint': [1.3, 0.85, 0.4, 1., 0., 0., 0.],
         }
-        env = fetch_env.FetchEnv('fetch/push_adversarial_clutter.xml', has_object=True, block_gripper=True, n_substeps=20,
+        env = fetch_env.FetchEnv('fetch/push_extreme_clutter.xml', has_object=True, block_gripper=True, n_substeps=20,
             gripper_extra_height=0.2, target_in_the_air=False, target_offset=0.0,
             obj_range=0.15, target_range=0.15, distance_threshold=0.05,
             initial_qpos=initial_qpos, reward_type='sparse')
