09bcabedd309b8b1cfaf51d9a5d27aa4fa119908
julianalverio <julian.a.alverio@gmail.com>
2018-12-18 18:01:01 -0500

    iterating

--- fetch/gripper_enabled/gym/envs/robotics/robot_env.py
+++ fetch/gripper_enabled/gym/envs/robotics/robot_env.py
@@ -57,6 +57,7 @@ class RobotEnv(gym.GoalEnv):
     def step(self, action):
         action = np.clip(action, self.action_space.low, self.action_space.high)
         self._set_action(action)
+        import pdb; pdb.set_trace()
         self.sim.step()
         self._step_callback()
         obs = self._get_obs()
