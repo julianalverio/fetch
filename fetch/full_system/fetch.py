d2c0345d83dc3b2025e59e1acc8e5a6b65f1e6d2
julianalverio <julian.a.alverio@gmail.com>
2019-01-20 21:34:25 -0800

    iterating

--- fetch/full_system/fetch.py
+++ fetch/full_system/fetch.py
@@ -617,13 +617,13 @@ def cleanup():
     for csv_txt_file in csv_txt_files:
         os.remove(csv_txt_file)
 
-def seed():
-    seed = random.randrange(0, 100)
-    np.random.seed(seed)
-    random.seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
+# def seed():
+#     seed = random.randrange(0, 100)
+#     np.random.seed(seed)
+#     random.seed(seed)
+#     torch.backends.cudnn.deterministic = True
+#     torch.manual_seed(seed)
+#     torch.cuda.manual_seed_all(seed)
 
 
 if __name__ == "__main__":
