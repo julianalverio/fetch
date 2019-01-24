a449e5b4c8fb43e3e1bd93bad508afd419894a7b
julianalverio <julian.a.alverio@gmail.com>
2019-01-24 08:19:25 -0800

    iterating

--- fetch/full_system/fetch.py
+++ fetch/full_system/fetch.py
@@ -428,8 +428,6 @@ class Trainer(object):
             print('Task:', self.task)
             self.reset()
             for iteration in range(MAX_ITERATIONS):
-                import pdb;
-                pdb.set_trace()
                 # execute one move
                 frame_idx += 1
                 reward, done = self.addExperience()
