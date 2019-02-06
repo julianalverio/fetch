f07eae563318cbee53794d5e18ba1e4aa1c785e5
julianalverio <julian.a.alverio@gmail.com>
2019-02-06 11:59:28 -0500

    iterating

--- fetch/full_system/bitflipping/her.py
+++ fetch/full_system/bitflipping/her.py
@@ -106,10 +106,8 @@ def updateTarget(op_holder,sess):
 def main():
     tb_writer = SummaryWriter('results')
     HER = True
-    shaped_reward = False
     size = 15
-    num_epochs = 5
-    num_cycles = 50
+    num_epochs = 250
     num_episodes = 16
     optimisation_steps = 40
     K = 4
@@ -119,11 +117,9 @@ def main():
     epsilon = 0.0
     batch_size = 128
 
-    total_rewards = []
     total_loss = []
     success_rate = []
     succeed = 0
-    train = True
 
     modelNetwork = Model(size = size, name = "model")
     targetNetwork = Model(size = size, name = "target")
@@ -134,82 +130,77 @@ def main():
     episode_count = 0
     import time
     start = time.time()
-    if train:
-        with tf.Session() as sess:
-            sess.run(modelNetwork.init_op)
-            sess.run(targetNetwork.init_op)
-            for _ in range(num_epochs):
-                for j in range(num_cycles):
-                    total_reward = 0.0
-                    successes = []
-                    for n in range(num_episodes):
-                        episode_count += 1
-                        env.reset()
-                        episode_experience = []
-                        episode_succeeded = False
-                        for t in range(size):
-                            s = np.copy(env.state)
-                            g = np.copy(env.target)
-                            inputs = np.concatenate([s,g],axis = -1)
-                            action = sess.run(modelNetwork.predict,feed_dict = {modelNetwork.inputs:[inputs]})
-                            action = action[0]
-                            if np.random.rand(1) < epsilon:
-                                action = np.random.randint(size)
-                            s_next, reward = env.step(action)
-                            episode_experience.append((s,action,reward,s_next,g))
-                            total_reward += reward
-                            if reward == 0:
-                                if episode_succeeded:
-                                    continue
-                                else:
-                                    episode_succeeded = True
-                                    succeed += 1
+    with tf.Session() as sess:
+        sess.run(modelNetwork.init_op)
+        sess.run(targetNetwork.init_op)
+        for _ in range(num_epochs):
+            total_reward = 0.0
+            successes = []
+            for n in range(num_episodes):
+                episode_count += 1
+                env.reset()
+                episode_experience = []
+                episode_succeeded = False
+                for t in range(size):
+                    s = np.copy(env.state)
+                    g = np.copy(env.target)
+                    inputs = np.concatenate([s,g],axis=-1)
+                    action = sess.run(modelNetwork.predict,feed_dict = {modelNetwork.inputs:[inputs]})
+                    action = action[0]
+                    if np.random.rand(1) < epsilon:
+                        action = np.random.randint(size)
+                    s_next, reward = env.step(action)
+                    episode_experience.append((s,action,reward,s_next,g))
+                    total_reward += reward
+                    if reward == 0:
                         if episode_succeeded:
-                            tb_writer.add_scalar('Success', 1, episode_count)
+                            continue
                         else:
-                            tb_writer.add_scalar('Success', 0, episode_count)
-                        successes.append(episode_succeeded)
-                        for t in range(size):
-                            s, a, r, s_n, g = episode_experience[t]
-                            inputs = np.concatenate([s,g], axis=-1)
-                            new_inputs = np.concatenate([s_n,g], axis=-1)
-                            buff.add(np.reshape(np.array([inputs, a, r, new_inputs]), [1, 4]))
-                            if HER:
-                                for k in range(K):
-                                    future = np.random.randint(t, size)
-                                    _, _, _, g_n, _ = episode_experience[future]
-                                    inputs = np.concatenate([s,g_n],axis = -1)
-                                    new_inputs = np.concatenate([s_n, g_n],axis = -1)
-                                    final = np.sum(np.array(s_n) == np.array(g_n)) == size
-                                    if shaped_reward:
-                                        r_n = 0 if final else -np.sum(np.square(np.array(s_n) == np.array(g_n)))
-                                    else:
-                                        r_n = 0 if final else -1
-                                    buff.add(np.reshape(np.array([inputs,a,r_n,new_inputs]),[1,4]))
+                            episode_succeeded = True
+                            succeed += 1
+                if episode_succeeded:
+                    tb_writer.add_scalar('Success', 1, episode_count)
+                else:
+                    tb_writer.add_scalar('Success', 0, episode_count)
+                successes.append(episode_succeeded)
+                for t in range(size):
+                    s, a, r, s_n, g = episode_experience[t]
+                    inputs = np.concatenate([s,g], axis=-1)
+                    new_inputs = np.concatenate([s_n,g], axis=-1)
+                    buff.add(np.reshape(np.array([inputs, a, r, new_inputs]), [1, 4]))
+                    if HER:
+                        for k in range(K):
+                            future = np.random.randint(t, size)
+                            _, _, _, g_n, _ = episode_experience[future]
+                            inputs = np.concatenate([s,g_n],axis = -1)
+                            new_inputs = np.concatenate([s_n, g_n],axis = -1)
+                            final = np.sum(np.array(s_n) == np.array(g_n)) == size
+                            r_n = 0 if final else -1
+                            buff.add(np.reshape(np.array([inputs,a,r_n,new_inputs]),[1,4]))
 
-                    mean_loss = []
-                    for k in range(optimisation_steps):
-                        experience = buff.sample(batch_size)
-                        s, a, r, s_next = [np.squeeze(elem, axis=1) for elem in np.split(experience, 4, 1)]
-                        s = np.array([ss for ss in s])
-                        s = np.reshape(s, (batch_size, size * 2))
-                        s_next = np.array([ss for ss in s_next])
-                        s_next = np.reshape(s_next, (batch_size, size * 2))
-                        Q1 = sess.run(modelNetwork.Q_, feed_dict = {modelNetwork.inputs: s_next})
-                        Q2 = sess.run(targetNetwork.Q_, feed_dict = {targetNetwork.inputs: s_next})
-                        doubleQ = Q2[:, np.argmax(Q1, axis = -1)]
-                        Q_target = np.clip(r + gamma * doubleQ,  -1. / (1 - gamma), 0)
-                        _, loss = sess.run([modelNetwork.train_op, modelNetwork.loss], feed_dict = {modelNetwork.inputs: s, modelNetwork.Q_next: Q_target, modelNetwork.action: a})
-                        mean_loss.append(loss)
+            mean_loss = []
+            for k in range(optimisation_steps):
+                experience = buff.sample(batch_size)
+                s, a, r, s_next = [np.squeeze(elem, axis=1) for elem in np.split(experience, 4, 1)]
+                s = np.array([ss for ss in s])
+                s = np.reshape(s, (batch_size, size * 2))
+                s_next = np.array([ss for ss in s_next])
+                s_next = np.reshape(s_next, (batch_size, size * 2))
+                Q1 = sess.run(modelNetwork.Q_, feed_dict = {modelNetwork.inputs: s_next})
+                Q2 = sess.run(targetNetwork.Q_, feed_dict = {targetNetwork.inputs: s_next})
+                doubleQ = Q2[:, np.argmax(Q1, axis = -1)]
+                Q_target = np.clip(r + gamma * doubleQ,  -1. / (1 - gamma), 0)
+                _, loss = sess.run([modelNetwork.train_op, modelNetwork.loss], feed_dict = {modelNetwork.inputs: s, modelNetwork.Q_next: Q_target, modelNetwork.action: a})
+                mean_loss.append(loss)
 
-                    success_rate.append(np.mean(successes))
-                    total_loss.append(np.mean(mean_loss))
-                    updateTarget(updateOps,sess)
-                    total_rewards.append(total_reward)
-        print("Number of episodes succeeded: {}".format(succeed))
-        print('TOTAL EPISODES', episode_count)
-        end = time.time()
-        print('time difference:', end - start)
+            success_rate.append(np.mean(successes))
+            total_loss.append(np.mean(mean_loss))
+            updateTarget(updateOps,sess)
+    print("Number of episodes succeeded: {}".format(succeed))
+    print('TOTAL EPISODES', episode_count)
+    end = time.time()
+    print('time difference:', end - start)
+
 
 def cleanup():
     if os.path.isdir('results_continuous'):
---
+++ fetch/full_system/her/fetch.py
@@ -0,0 +1,403 @@
+#!/usr/bin/env python3
+import random
+import numpy as np
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import copy
+from collections import namedtuple
+from torch.autograd import Variable
+import cv2
+import argparse
+from tensorboardX import SummaryWriter
+import shutil
+import os
+from gym.envs.robotics import fetch_env
+from gym.wrappers.time_limit import TimeLimit
+from replay_buffer import PrioritizedReplayBuffer, ReplayBuffer
+from gym.envs.robotics.fetch.pick_and_place import FetchPickAndPlaceEnv
+from gym.envs.robotics.fetch.push import FetchPushEnv
+from gym.envs.robotics.fetch.slide import FetchSlideEnv
+from gym.envs.robotics.fetch.reach import FetchReachEnv
+
+NUM_EPISODES = 3000
+MAX_ITERATIONS = 700
+
+# Actions:
+# 0 -- increment X
+# 1 -- decrement X
+# 2 -- increment Y
+# 3 -- decrement Y
+# 4 -- increment Z
+# 5 -- decrement Z
+# 6 -- continuously try to open gripper
+# 7 -- continuously try to close gripper
+
+HYPERPARAMS = {
+        'replay_size':      8000,
+        'replay_initial':   7900,
+        'target_net_sync':  1000,
+        'epsilon_frames':   10**5,
+        'epsilon_start':    1.0,
+        'epsilon_final':    0.02,
+        'learning_rate':    0.0001,
+        'gamma':            0.99,
+        'batch_size':       32
+}
+
+
+class DuelingDQN(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        super(DuelingDQN, self).__init__()
+        self.num_actions = num_actions
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+        conv_out = self.conv(Variable(torch.zeros(1, *input_shape)))
+        conv_out_size = int(np.prod(conv_out.size()))
+
+        self.fc_adv = nn.Sequential(
+            nn.Linear(in_features=conv_out_size + 1, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=num_actions)
+        )
+
+        self.fc_val = nn.Sequential(
+            nn.Linear(in_features=conv_out_size + 1, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=1)
+        )
+
+    def forward(self, state_and_goal):
+        state, goal = state_and_goal
+        state = self.conv(state)
+        state = state.view(state.size(0), -1)
+        x = torch.cat([state, goal], dim=1)
+
+        adv = self.fc_adv(x)
+        val = self.fc_val(x).expand(x.size(0), self.num_actions)
+        return val + adv - adv.mean(1).unsqueeze(1).expand(x.size(0), self.num_actions)
+
+
+class DQN(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        super(DQN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out = self.conv(Variable(torch.zeros(1, *input_shape)))
+        conv_out_size = int(np.prod(conv_out.size()))
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size + 1, 512),
+            nn.ReLU(),
+            nn.Linear(512, num_actions)
+        )
+
+    def forward(self, state_and_goal):
+        state, goal = state_and_goal
+        state = self.conv(state)
+        state = state.view(state.size(0), -1)
+        x = torch.cat([state, goal], dim=1)
+        return self.fc(x)
+
+
+class ValueTracker(object):
+    def __init__(self, length=20):
+        self.length = length
+        self.values = []
+        self.position = 0
+        self.mean = 0
+
+    def add(self, value):
+        if len(self.values) < self.length:
+            self.values.append(value)
+        else:
+            self.values[self.position] = value
+            self.position = (self.position + 1) % self.length
+        self.mean = np.mean(self.values)
+
+    def getMean(self):
+        return self.mean
+
+
+class LinearScheduler(object):
+    def __init__(self, start, stop, delta=None, timespan=None):
+        assert delta or timespan
+        self.value = start
+        self.stop = stop
+        if delta:
+            self.delta = float(delta)
+        elif timespan:
+            self.delta = (stop - start) / float(timespan)
+
+    def updateAndGetValue(self):
+        self.value += self.delta
+        self.observeValue()
+
+    def observeValue(self):
+        if self.delta > 0:
+            return min(self.value, self.stop)
+        else:
+            return max(self.value, self.stop)
+
+
+# TODO: add support for extensions: Dueling, PER, HER
+# TODO: add a pick and place environment where the starting position can also change
+class Trainer(object):
+    def __init__(self):
+        self.params = HYPERPARAMS
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.action_space = 8
+        self.observation_space = [3, 102, 205]
+
+        self.envs = self.makeEnvs()
+        self.env = None
+
+        self.policy_net = DQN(self.observation_space, self.action_space).to(self.device)
+        self.target_net = copy.deepcopy(self.policy_net)
+
+        self.epsilon_scheduler = LinearScheduler(self.params['epsilon_start'], self.params['epsilon_final'], self.params['epsilon_frames'])
+        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=self.params['learning_rate'])
+        self.reward_trackers = [ValueTracker() for _ in range(len(self.envs))]
+        self.episode_counters = [0] * len(self.envs)
+        self.memory = ReplayBuffer(self.params['replay_size'])
+        self.tb_writer = SummaryWriter('results')
+
+        self.gripper_states = [0] * len(self.envs)  # 0 for opening, 1 for closing
+        self.task = 0
+        self.place_env_idx = None
+
+    def makeEnvs(self):
+        envs = list()
+        # envs.append(FetchPickAndPlaceEnv())
+        # envs.append(FetchSlideEnv)
+        # envs.append(FetchPushEnv())
+        envs.append(FetchReachEnv())
+        # envs.append(FetchPickAndPlaceEnv(target_in_the_air=False))
+        # self.place_env_idx = len(envs) - 1
+        return envs
+
+    # there are some additional movements here to compensate for momentum
+    def resetforPlacing(self, env):
+        object_position = env.sim.data.get_site_xpos('object0')
+        gripper_position = env.sim.data.get_site_xpos('robot0:grip')
+        starting_position = copy.deepcopy(gripper_position)
+
+        # Get x just right
+        while gripper_position[0] > object_position[0]:
+            env.step([-1, 0, 0, 0])
+            env.render()
+        while gripper_position[0] < object_position[0]:
+            env.step([1, 0, 0, 0])
+            env.render()
+
+        # Get y just right
+        while gripper_position[1] > object_position[1]:
+            env.step([0, -1, 0, 0])
+            env.render()
+        while gripper_position[1] < object_position[1]:
+            env.step([0, 1, 0, 0])
+            env.render()
+
+        env.move([0, 0, 0, 1], count=10)  # open
+        env.move([0, 0, -1, 1], count=20)  # drop
+        env.move([0, 0, 0, -1], count=15)  # close
+
+        # get z right
+        while gripper_position[2] < starting_position[2]:
+            env.step([0, 0, 1, -1])
+        env.step([0, 0, -1, -1])
+        # get y right
+        while gripper_position[1] < starting_position[1]:
+            env.step([0, 1, 0, -1])
+        env.step([0, -1, 0, -1])
+        while gripper_position[1] > starting_position[1]:
+            env.step([0, -1, 0, -1])
+        env.step([0, 1, 0, -1])
+        # get x right
+        while gripper_position[0] > starting_position[0]:
+            env.step([-1, 0, 0 -1])
+        env.step([1, 0, 0 -1])
+        while gripper_position[0] < starting_position[0]:
+            env.step([1, 0, 0 - 1])
+        env.step([-1, 0, 0 - 1])
+
+    def reset(self):
+        self.task = random.randrange(0, len(self.envs))
+        print('Task:', self.task)
+        env = self.envs[self.task]
+        env.reset()
+        if self.task == self.place_env_idx:
+            self.resetforPlacing(env)
+        env.renderalot()
+        self.env = env
+        self.gripper_states[self.task] = 0
+
+    def preprocess(self, state):
+        import pdb; pdb.set_trace()
+        state = state[230:435, 50:460]
+        state = cv2.resize(state, (state.shape[1]//2, state.shape[0]//2), interpolation=cv2.INTER_AREA).astype(np.float32)/256
+        state = np.swapaxes(state, 0, 2)
+        return torch.tensor(state, device=self.device).unsqueeze(0)
+
+    # indices are x, y, z, gripper
+    def convertAction(self, action):
+        movement = np.zeros(4)
+        if action.item() % 2 == 0:
+            movement[action.item() // 2] += 1
+        else:
+            movement[action.item() // 2] -= 1
+        if action.item() == 6:
+            self.gripper_states[self.task] = 0
+        elif action.item() == 7:
+            self.gripper_states[self.task] = 1
+        if self.gripper_states[self.task] == 0:
+            movement[-1] = 1
+        elif self.gripper_states[self.task] == 1:
+            movement[-1] = -1
+        return movement
+
+    def prepareState(self, goal_prime=None):
+        state, goal = self.env.getStateAndGoal()
+        if goal_prime:
+            goal = goal_prime
+        state = torch.tensor(self.preprocess(state), device=self.device)
+        goal = torch.tensor(goal)
+        return np.array([state, goal], dtype=np.object)
+
+    def addExperience(self):
+        state = self.prepareState()
+        if random.random() < self.epsilon_scheduler.updateAndGetValue():
+            action = torch.tensor([random.randrange(self.action_space)], device=self.device)
+        else:
+            action = torch.argmax(self.policy_net(state), dim=1).to(self.device)
+        action_converted = self.convertAction(action)
+        self.env.step(action_converted)
+        next_state = self.prepareState()
+        reward = torch.tensor([self.env.getGoalAchievedAndReward()], device=self.device)  # 0 or -1
+
+        self.memory.add(self.state, action, reward, next_state, reward * 1)
+        return reward, reward == 0
+
+    def optimizeModel(self):
+        states, actions, rewards, next_states, _ = self.memory.sample(self.params['batch_size'])
+        states = torch.tensor(states, device=self.device)
+        actions = torch.tensor(actions, device=self.device)
+        rewards = torch.tensor(rewards, device=self.device)
+        next_states = torch.tensor(next_states, device=self.device)
+        state_action_values = self.policy_net(states).gather(1, actions.unsqueeze(1))
+        next_state_values = self.target_net(next_states).max(1)[0].detach()
+        expected_state_action_values = (next_state_values * self.params['gamma']) + rewards
+        loss = nn.MSELoss()(state_action_values, expected_state_action_values.unsqueeze(1))
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+
+    # def optimizeModelPER(self):
+    #     # transitions, ISWeights, tree_idx = self.memory.sample(self.batch_size)
+    #     ISWeights = torch.tensor(ISWeights, device=self.device)
+    #     batch = self.transition(*zip(*transitions))
+    #     next_states = batch.next_state
+    #     non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, next_states)), device=self.device, dtype=torch.uint8)
+    #     non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+    #     non_final_tasks = torch.cat([batch.task[i] for i in range(self.params['batch_size']) if next_states[i] is not None])
+    #     state_batch = torch.cat(list(batch.state))
+    #     action_batch = torch.cat(list(batch.action))
+    #     reward_batch = torch.cat(list(batch.reward))
+    #     task_batch = torch.cat(list(batch.task))
+    #     state_action_values = self.policy_net(state_batch, task_batch).gather(1, action_batch.unsqueeze(1))
+    #     next_state_values = torch.zeros(self.params['batch_size'], device=self.device)
+    #     next_state_values[non_final_mask] = self.target_net(non_final_next_states, non_final_tasks).max(1)[0].detach()
+    #     expected_state_action_values = (next_state_values * self.params['gamma']) + reward_batch
+    #     abs_errors = abs(expected_state_action_values.unsqueeze(1) - state_action_values)
+    #     loss = torch.sum((abs_errors ** 2) * ISWeights)
+    #     self.memory.batch_update(tree_idx, abs_errors.detach().cpu().numpy() + 1e-6)
+    #     # loss = nn.MSELoss()(state_action_values, expected_state_action_values.unsqueeze(1))
+    #     self.optimizer.zero_grad()
+    #     loss.backward()
+    #     self.optimizer.step()
+
+    def logEpisode(self, iteration, reward):
+        self.reward_trackers[self.task].add(reward)
+        self.tb_writer.add_scalar('Steps per episode | task=%s' % self.task, iteration, self.episode_counters[self.task])
+        self.tb_writer.add_scalar('Score | task=%s' % self.task, reward, self.episode_counters[self.task])
+        self.tb_writer.add_scalar('Average Score | task=%s' % self.task, self.reward_trackers[self.task].getMean(), self.episode_counters[self.task])
+        self.tb_writer.add_scalar('Epsilon', self.epsilon_scheduler.observeValue(), sum(self.episode_counters))
+        self.episode_counters[self.task] += 1
+
+    def prefetch(self):
+        while 1:
+            self.reset()
+            for iteration in range(MAX_ITERATIONS):
+                reward = self.addExperience()
+                done = reward == 0
+                if len(self.memory) >= self.params['replay_initial']:
+                    print('Done Prefetching.')
+                    return
+                if done:
+                    break
+
+    def train(self):
+        self.prefetch()
+        frame_idx = 0
+        for episode in range(NUM_EPISODES):
+            self.reset()
+            for iteration in range(MAX_ITERATIONS):
+                reward, done = self.addExperience()
+                self.optimizeModel()
+                frame_idx += 1
+                if frame_idx % self.params['target_net_sync'] == 0:
+                    self.target_net.load_state_dict(self.policy_net.state_dict())
+
+                if done or iteration == MAX_ITERATIONS - 1:
+                    print('Episode Completed:', episode, 'Task:', self.task, 'Score:', reward)
+                    self.logEpisode(iteration, reward)
+                    break
+
+
+def cleanup():
+    if os.path.isdir('results_continuous'):
+        shutil.rmtree('results_continuous')
+    csv_txt_files = [x for x in os.listdir('.') if '.TXT' in x or '.csv' in x]
+    for csv_txt_file in csv_txt_files:
+        os.remove(csv_txt_file)
+
+# def seed():
+#     seed = random.randrange(0, 100)
+#     np.random.seed(seed)
+#     random.seed(seed)
+#     torch.backends.cudnn.deterministic = True
+#     torch.manual_seed(seed)
+#     torch.cuda.manual_seed_all(seed)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('gpu', type=int)
+    gpu_num = parser.parse_args().gpu
+    print('GPU:', gpu_num)
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_num)
+    cleanup()
+    print('Creating Trainer')
+    trainer = Trainer()
+    print('Trainer Initialized')
+    trainer.train()
+
+
+
+
+
