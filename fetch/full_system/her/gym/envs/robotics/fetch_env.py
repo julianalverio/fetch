9f01320976d411db9e49c847e03028a533d2c0bf
julianalverio <julian.a.alverio@gmail.com>
2019-02-06 17:02:07 -0500

    iterating

--- fetch/full_system/her/gym/envs/robotics/fetch_env.py
+++ fetch/full_system/her/gym/envs/robotics/fetch_env.py
@@ -195,5 +195,5 @@ class FetchEnv(robot_env.RobotEnv):
         else:
             goal_achieved = self.sim.data.get_site_xpos('robot0:grip')
         goal = self.goal.copy()
-        reward = self.compute_reward(goal_achieved, goal)
+        reward = self.compute_reward(goal_achieved, goal, dict())
         return reward
