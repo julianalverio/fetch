28039e56dab103a67d301f182d39626929157a8e
julianalverio <julian.a.alverio@gmail.com>
2019-02-06 16:36:52 -0500

    iterating

--- fetch/full_system/her/fetch.py
+++ fetch/full_system/her/fetch.py
@@ -243,7 +243,6 @@ class Trainer(object):
         self.env.renderalot()
 
     def preprocess(self, state):
-        import pdb; pdb.set_trace()
         state = state[230:435, 50:460]
         state = cv2.resize(state, (state.shape[1]//2, state.shape[0]//2), interpolation=cv2.INTER_AREA).astype(np.float32)/256
         state = np.swapaxes(state, 0, 2)
