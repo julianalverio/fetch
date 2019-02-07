0c85e325b9b7ca223126f2c22f720b5d8b00cbdb
julianalverio <julian.a.alverio@gmail.com>
2019-02-07 12:12:00 -0500

    iterating

--- fetch/full_system/her/fetch.py
+++ fetch/full_system/her/fetch.py
@@ -282,7 +282,7 @@ class Trainer(object):
         goal_zeros = np.zeros([1, 205, 102], dtype=np.float32)
         goal_zeros[0, 0, 0:3] = goal
         goal = torch.tensor(goal_zeros, device=self.device)
-        return torch.cat([state, goal], dim=1)
+        return torch.cat([state, goal], dim=0)
 
     def addExperience(self):
         state = self.prepareState()
