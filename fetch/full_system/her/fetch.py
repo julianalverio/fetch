a5b3cbbdf123b14d7e1186f39e9f5d8b7a9cf390
julianalverio <julian.a.alverio@gmail.com>
2019-02-08 15:21:47 -0500

    iterating

--- fetch/full_system/her/fetch.py
+++ fetch/full_system/her/fetch.py
@@ -17,23 +17,14 @@ import sys
 import copy
 sys.path.insert(0, '..')
 sys.path.insert(0, '.')
-import pdb; pdb.set_trace()
-try:
-    from gym.envs.robotics import fetch_env
-    from gym.wrappers.time_limit import TimeLimit
-    from replay_buffer import PrioritizedReplayBuffer, ReplayBuffer
-    from gym.envs.robotics.fetch.pick_and_place import FetchPickAndPlaceEnv
-    from gym.envs.robotics.fetch.push import FetchPushEnv
-    from gym.envs.robotics.fetch.slide import FetchSlideEnv
-    from gym.envs.robotics.fetch.reach import FetchReachEnv
-except:
-    from gym.envs.robotics import fetch_env
-    from gym.wrappers.time_limit import TimeLimit
-    from replay_buffer import PrioritizedReplayBuffer, ReplayBuffer
-    from gym.envs.robotics.fetch.pick_and_place import FetchPickAndPlaceEnv
-    from gym.envs.robotics.fetch.push import FetchPushEnv
-    from gym.envs.robotics.fetch.slide import FetchSlideEnv
-    from gym.envs.robotics.fetch.reach import FetchReachEnv
+from gym.envs.robotics import fetch_env
+from gym.wrappers.time_limit import TimeLimit
+from replay_buffer import PrioritizedReplayBuffer, ReplayBuffer
+from gym.envs.robotics.fetch.pick_and_place import FetchPickAndPlaceEnv
+from gym.envs.robotics.fetch.push import FetchPushEnv
+from gym.envs.robotics.fetch.slide import FetchSlideEnv
+from gym.envs.robotics.fetch.reach import FetchReachEnv
+
 
 
 
