e2b5f337fedc622cc1b1186d7e1a5f724b80fab3
julianalverio <julian.a.alverio@gmail.com>
2019-02-10 21:50:36 -0500

    iterating

--- fetch/full_system/her/gym/envs/robotics/robot_env.py
+++ fetch/full_system/her/gym/envs/robotics/robot_env.py
@@ -14,26 +14,6 @@ except ImportError as e:
 
 class RobotEnv(gym.GoalEnv):
     def __init__(self, model_path, initial_qpos, n_actions, n_substeps):
-        # if model_path.startswith('/'):
-        #     fullpath = model_path
-        # else:
-        #     fullpath = os.path.join(os.path.dirname(__file__), 'assets', model_path)
-        # if not os.path.exists(fullpath):
-        #     raise IOError('File {} does not exist'.format(fullpath))
-        #
-        # model = mujoco_py.load_model_from_path(fullpath)
-        # self.sim = mujoco_py.MjSim(model, nsubsteps=n_substeps)
-        # self.viewer = None
-        #
-        # self.metadata = {
-        #     'render.modes': ['human', 'rgb_array'],
-        #     'video.frames_per_second': int(np.round(1.0 / self.dt))
-        # }
-        #
-        # self.seed()
-        # self._env_setup(initial_qpos=initial_qpos)
-        # self.initial_state = copy.deepcopy(self.sim.get_state())
-
         if model_path.startswith('/'):
             fullpath = model_path
         else:
@@ -42,7 +22,7 @@ class RobotEnv(gym.GoalEnv):
             raise IOError('File {} does not exist'.format(fullpath))
 
         model = mujoco_py.load_model_from_path(fullpath)
-        self.sim = mujoco_py.MjSim(model, nsubsteps=2)
+        self.sim = mujoco_py.MjSim(model, nsubsteps=20)
         self.viewer = None
 
         self.metadata = {
