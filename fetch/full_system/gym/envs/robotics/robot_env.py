77643e82a2c3fa4a2c88ebe1eebf3d2723bcd52e
julianalverio <julian.a.alverio@gmail.com>
2019-02-10 20:57:39 -0500

    iterating

--- fetch/full_system/gym/envs/robotics/robot_env.py
+++ fetch/full_system/gym/envs/robotics/robot_env.py
@@ -34,14 +34,14 @@ class RobotEnv(gym.GoalEnv):
         self._env_setup(initial_qpos=initial_qpos)
         self.initial_state = copy.deepcopy(self.sim.get_state())
 
-        self.goal = self._sample_goal()
-        obs = self._get_obs()
-        self.action_space = spaces.Box(-1., 1., shape=(n_actions,), dtype='float32')
-        self.observation_space = spaces.Dict(dict(
-            desired_goal=spaces.Box(-np.inf, np.inf, shape=obs['achieved_goal'].shape, dtype='float32'),
-            achieved_goal=spaces.Box(-np.inf, np.inf, shape=obs['achieved_goal'].shape, dtype='float32'),
-            observation=spaces.Box(-np.inf, np.inf, shape=obs['observation'].shape, dtype='float32'),
-        ))
+        # self.goal = self._sample_goal()
+        # obs = self._get_obs()
+        # self.action_space = spaces.Box(-1., 1., shape=(n_actions,), dtype='float32')
+        # self.observation_space = spaces.Dict(dict(
+        #     desired_goal=spaces.Box(-np.inf, np.inf, shape=obs['achieved_goal'].shape, dtype='float32'),
+        #     achieved_goal=spaces.Box(-np.inf, np.inf, shape=obs['achieved_goal'].shape, dtype='float32'),
+        #     observation=spaces.Box(-np.inf, np.inf, shape=obs['observation'].shape, dtype='float32'),
+        # ))
 
     @property
     def dt(self):
