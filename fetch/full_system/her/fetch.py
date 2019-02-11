f5114bb9de0084b80bb01c780e883909b1d7408c
julianalverio <julian.a.alverio@gmail.com>
2019-02-10 21:53:40 -0500

    iterating

--- fetch/full_system/her/fetch.py
+++ fetch/full_system/her/fetch.py
@@ -24,6 +24,7 @@ from gym.envs.robotics.fetch.pick_and_place import FetchPickAndPlaceEnv
 from gym.envs.robotics.fetch.push import FetchPushEnv
 from gym.envs.robotics.fetch.slide import FetchSlideEnv
 from gym.envs.robotics.fetch.reach import FetchReachEnv
+from PIL import Image
 
 
 # Actions:
