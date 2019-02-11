b5f87822bc0424928299ef8696026d0bb4b0c9eb
julianalverio <julian.a.alverio@gmail.com>
2019-02-10 22:02:36 -0500

    iterating

--- fetch/full_system/her/fetch.py
+++ fetch/full_system/her/fetch.py
@@ -207,6 +207,7 @@ class Trainer(object):
         self.env = self.envs[self.task]
         # self.env.sim.nsubsteps = 20
         # self.env.gain = 0.05
+        import pdb; pdb.set_trace()
         self.env.reset()
         self.env.sim.nsubsteps = 2
         # self.env.gain = 0.2
