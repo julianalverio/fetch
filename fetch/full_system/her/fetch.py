620318e9b9241812faab55b9ab1555f2a4927158
julianalverio <julian.a.alverio@gmail.com>
2019-02-17 17:29:22 -0500

    iterating

--- fetch/full_system/her/fetch.py
+++ fetch/full_system/her/fetch.py
@@ -327,6 +327,7 @@ class Trainer(object):
     def preprocess(self, state):
         if random.random() > 0.1:
             Image.fromarray(state).show()
+        import pdb; pdb.set_trace()
         state = state[230:435, 50:460]
         state = cv2.resize(state, (state.shape[1]//4, state.shape[0]//2), interpolation=cv2.INTER_AREA).astype(np.float32)/256
         state = np.swapaxes(state, 0, 2)
