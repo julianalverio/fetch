83105e18a2bcce0b5a46d70081b10c818d0f8a23
julianalverio <julian.a.alverio@gmail.com>
2019-02-18 15:41:31 -0500

    iterating

--- fetch/full_system/her/fetch.py
+++ fetch/full_system/her/fetch.py
@@ -329,8 +329,9 @@ class Trainer(object):
 
     def preprocess(self, state):
         state = state[180:435, 50:460]
-        Image.fromarray(state).show()
-        import pdb; pdb.set_trace()
+        if random.random() > 0.995:
+            Image.fromarray(state).show()
+            import pdb; pdb.set_trace()
         state = cv2.resize(state, (state.shape[1]//4, state.shape[0]//4), interpolation=cv2.INTER_AREA).astype(np.float32)/256
         state = np.swapaxes(state, 0, 2)
         return torch.tensor(state, device=self.device).unsqueeze(0)
